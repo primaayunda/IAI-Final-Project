ed08a32c256c2d6277d6d4314eac6e3081f51f14
primaayunda <primariliayunda@gmail.com>
2021-04-03 17:09:15 +0700

    hufttttttttt

--- finalproject.py
+++ finalproject.py
@@ -5,18 +5,17 @@ from email.mime.text import MIMEText
 
 sender = str(input("Please input your username: "))
 password = getpass.getpass('Please input your password: ')
-# receiver = str(input("Please input email address: "))
-with open ("receiver_list.txt", "r") as file:
-    receiver = file.read().replace('\n','')
-# print(receiver.read())
-# receiver = open("receiver_list.txt", "r+")
-# receiver_list = [i.strip() for i in receiver.readlines()]
+
+receiver = open ("receiver_list.txt", "r")
+receiverlist = []
+for i in receiver:
+    i = i.replace("\n", "")
+    receiverlist.append(i.split(" "))
+print(receiverlist)
 
 message = MIMEMultipart()
 message ['From'] = sender
-# for addr in receiver:
-#     message ['To'] = addr
-message ['To'] = receiver
+message ['To'] = i
 message ['Subject'] = str(input("Input your mail subject: "))
 
 body = str(input("Input your mail body: "))
@@ -25,16 +24,9 @@ message.attach(MIMEText(body, 'plain'))
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 
-# a = (server.login(sender, password)
-# server.sendmail(sender, receiver, message.as_string())
-# server.quit)
-
-# for a in receiver (open ("receiver_list.txt", "r")):
-#     print("Login success")
-#     print("Email has been sent to ", receiver)
-
-server.login(sender, password)
-print("Login success")
-server.sendmail(sender, receiver, message.as_string())
-server.quit
-print("Email has been sent to ", receiver)
+for email in receiverlist:
+    server.login(sender, password)
+    print("Login success")
+    server.sendmail(sender, receiverlist, message.as_string())
+server.quit()
+print("Email has been sent to ", receiverlist)
